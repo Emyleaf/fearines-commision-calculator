e7e4779bcb90c2126d2a14cb3df22b3d074e3462
Emyleaf <emyleaf@gmail.com>
2021-04-05 19:11:19 +0200

    some changes

--- js/carrello.js
+++ js/carrello.js
@@ -4,7 +4,11 @@ function checkTotal() {
     var price3 = document.querySelector('input[name="backorno"]:checked').value;
     var price4 = document.querySelector('input[name="howmanypg"]').value;
 
-    var totale = parseInt(price1) + parseInt(price2) + parseInt(price3) +  parseInt(price4)*5;
+    if (parseInt(price4) > 5) {
+        price4 = 5;
+    }
+
+    var totale = parseInt(price1) + parseInt(price2) + parseInt(price3) + parseInt(price4)*5;
 
     document.getElementById("carrello").innerHTML = "Il costo totale è: " + totale + " €"
 }
