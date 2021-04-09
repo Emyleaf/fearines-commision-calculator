d8837ac5166edeeec59a8e2e42f5c0488614aeed
Emyleaf <emyleaf@gmail.com>
2021-04-09 17:03:40 +0200

    change language with jquery

--- js/carrello.js
+++ js/carrello.js
@@ -12,19 +12,41 @@ function checkTotal() {
         if (parseInt(price3) == 10){
             var totalemin = parseInt(price1) + parseInt(price2) + parseInt(price3) + parseInt(price4)*5;
             var totalemax = parseInt(price1) + parseInt(price2) + parseInt(price3) + 15 + parseInt(price4)*5;
-            document.getElementById("carrello").innerHTML = "Il costo totale varia da: min " + totalemin + "€ a max " + totalemax + "€ in base ai dettagli del background!"
+            if($('#switch-lang').hasClass("inglese")){ 
+                document.getElementById("carrello").innerHTML = "<span lang='eng' style='display: none;'>The total cost ranges from " + totalemin + "€ to " + totalemax + "€ depending on the details of the background!</span><span lang='ita'>Il costo totale varia da: min " + totalemin + "€ a max " + totalemax + "€ in base ai dettagli del background!</span>"
+            } else {
+                document.getElementById("carrello").innerHTML = "<span lang='eng'>The total cost ranges from " + totalemin + "€ to " + totalemax + "€ depending on the details of the background!</span><span lang='ita' style='display: none;'>Il costo totale varia da: min " + totalemin + "€ a max " + totalemax + "€ in base ai dettagli del background!</span>"
+            }
         } else {   
             var totale = parseInt(price1) + parseInt(price2) + parseInt(price3) + parseInt(price4)*5;
-            document.getElementById("carrello").innerHTML = "Il costo totale è: " + totale + " €"
+            if($('#switch-lang').hasClass("inglese")){ 
+                document.getElementById("carrello").innerHTML = "<span lang='eng' style='display: none;'>Total price is: " + totale + " €</span><span lang='ita'>Il costo totale è: " + totale + " €</span>"
+            } else {
+                document.getElementById("carrello").innerHTML = "<span lang='eng'>Total price is: " + totale + " €</span><span lang='ita' style='display: none;'>Il costo totale è: " + totale + " €</span>"
+            }
         }
     }
-
 }
 
-function validationMaxpg(p1){
+function validationMaxpg(p1) {
+    var x = document.getElementById("erroretipo4");
     if(!p1){ 
-        document.getElementById("erroretipo4").innerHTML = "Inserisci un numero valido di personaggi!"
+        x.style.display = "block";
     } else {
-        document.getElementById("erroretipo4").innerHTML = ""
+        x.style.display = "none";
     }
 }
+
+$('[lang="ita"]').hide();
+
+$('#switch-lang').click(function() {
+    if($('#switch-lang').hasClass("italiano")){ 
+        $('#switch-lang').removeClass("italiano");
+        $('#switch-lang').addClass("inglese");
+    } else {
+        $('#switch-lang').removeClass("inglese");
+        $('#switch-lang').addClass("italiano");
+    }
+    $('[lang="ita"]').toggle();
+    $('[lang="eng"]').toggle();
+});
